57cfa00837f63d602d8d0938c5a164eb1e2f1dda
JonasKorte <mscdelirium@gmail.com>
2021-07-04 10:47:50 +0200

    Fixed README.md

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-<img src="https://github.com/JonasKorte/Aspyre-Engine/raw/master/icons/icon.png" alt="Aspyre Icon" width="128px"/>
+<img src="https://github.com/JonasKorte/Aspyre-Engine/raw/master/branding/icon.png" alt="Aspyre Icon" width="128px"/>
 <h1>Aspyre Engine</h1>
 <p>Aspyre Engine is an open-source, Vulkan based, 2D & 3D game engine.</p>
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -47,6 +47,145 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("/home/jonas/Aspyre-Engine/build/external/Qt-Advanced-Docking-System/cmake_install.cmake")
 endif()
 
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  if(EXISTS "$ENV{DESTDIR}/home/jonas/Aspyre-Engine/release/bin/x11/aspyre" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}/home/jonas/Aspyre-Engine/release/bin/x11/aspyre")
+    file(RPATH_CHECK
+         FILE "$ENV{DESTDIR}/home/jonas/Aspyre-Engine/release/bin/x11/aspyre"
+         RPATH "")
+  endif()
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "/home/jonas/Aspyre-Engine/release/bin/x11/aspyre")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+file(INSTALL DESTINATION "/home/jonas/Aspyre-Engine/release/bin/x11" TYPE EXECUTABLE FILES "/home/jonas/Aspyre-Engine/build/aspyre")
+  if(EXISTS "$ENV{DESTDIR}/home/jonas/Aspyre-Engine/release/bin/x11/aspyre" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}/home/jonas/Aspyre-Engine/release/bin/x11/aspyre")
+    file(RPATH_CHANGE
+         FILE "$ENV{DESTDIR}/home/jonas/Aspyre-Engine/release/bin/x11/aspyre"
+         OLD_RPATH "/home/jonas/Aspyre-Engine/build/x64/lib:/home/jonas/Qt/5.12.11/gcc_64/lib:"
+         NEW_RPATH "")
+    if(CMAKE_INSTALL_DO_STRIP)
+      execute_process(COMMAND "/usr/bin/strip" "$ENV{DESTDIR}/home/jonas/Aspyre-Engine/release/bin/x11/aspyre")
+    endif()
+  endif()
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "/home/jonas/Aspyre-Engine/release/lib/libvulkanplugin.a")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+file(INSTALL DESTINATION "/home/jonas/Aspyre-Engine/release/lib" TYPE STATIC_LIBRARY FILES "/home/jonas/Aspyre-Engine/build/libvulkanplugin.a")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "/home/jonas/Aspyre-Engine/release/lib/libgraphicsplugin.a")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+file(INSTALL DESTINATION "/home/jonas/Aspyre-Engine/release/lib" TYPE STATIC_LIBRARY FILES "/home/jonas/Aspyre-Engine/build/libgraphicsplugin.a")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "/home/jonas/Aspyre-Engine/release/lib/lib2dplugin.a")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+file(INSTALL DESTINATION "/home/jonas/Aspyre-Engine/release/lib" TYPE STATIC_LIBRARY FILES "/home/jonas/Aspyre-Engine/build/lib2dplugin.a")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "/home/jonas/Aspyre-Engine/release/lib/lib3dplugin.a")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+file(INSTALL DESTINATION "/home/jonas/Aspyre-Engine/release/lib" TYPE STATIC_LIBRARY FILES "/home/jonas/Aspyre-Engine/build/lib3dplugin.a")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "/home/jonas/Aspyre-Engine/release/lib/libeditorplugin.a")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+file(INSTALL DESTINATION "/home/jonas/Aspyre-Engine/release/lib" TYPE STATIC_LIBRARY FILES "/home/jonas/Aspyre-Engine/build/libeditorplugin.a")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "/home/jonas/Aspyre-Engine/release/lib/libecsplugin.a")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+file(INSTALL DESTINATION "/home/jonas/Aspyre-Engine/release/lib" TYPE STATIC_LIBRARY FILES "/home/jonas/Aspyre-Engine/build/libecsplugin.a")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "/home/jonas/Aspyre-Engine/release/lib/libsceneplugin.a")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+file(INSTALL DESTINATION "/home/jonas/Aspyre-Engine/release/lib" TYPE STATIC_LIBRARY FILES "/home/jonas/Aspyre-Engine/build/libsceneplugin.a")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "/home/jonas/Aspyre-Engine/release/lib/libcoreplugin.a")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+file(INSTALL DESTINATION "/home/jonas/Aspyre-Engine/release/lib" TYPE STATIC_LIBRARY FILES "/home/jonas/Aspyre-Engine/build/libcoreplugin.a")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "/home/jonas/Aspyre-Engine/release/res/icons/icon.ico;/home/jonas/Aspyre-Engine/release/res/icons/icon.png")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+file(INSTALL DESTINATION "/home/jonas/Aspyre-Engine/release/res/icons" TYPE FILE FILES
+    "/home/jonas/Aspyre-Engine/res/icons/icon.ico"
+    "/home/jonas/Aspyre-Engine/branding/icon.png"
+    )
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
